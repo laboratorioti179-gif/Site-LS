7b3decaf6edccef988f4fbc877a3e1c7603041fd
Jhow <laboratorioti179@gmail.com>
2026-02-04 19:56:22 -0300

    first commit

---
+++ site-ls/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-6xl font-black text-emerald-400">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  )
+}
